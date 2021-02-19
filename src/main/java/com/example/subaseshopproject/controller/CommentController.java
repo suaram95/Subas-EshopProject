21762653bd0ace729607628c421327cf424e0d99
suaram95 <aram/*java2020>
2021-02-19 12:25:11 +0400

    changings in CommentController and singleBlog html

--- src/main/java/com/example/subaseshopproject/controller/CommentController.java
+++ src/main/java/com/example/subaseshopproject/controller/CommentController.java
@@ -1,8 +1,10 @@
 package com.example.subaseshopproject.controller;
 
 import com.example.subaseshopproject.dto.CommentRequestDto;
+import com.example.subaseshopproject.model.Blog;
 import com.example.subaseshopproject.model.Comment;
 import com.example.subaseshopproject.model.User;
+import com.example.subaseshopproject.service.BlogService;
 import com.example.subaseshopproject.service.CommentService;
 import com.example.subaseshopproject.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +22,12 @@ public class CommentController {
 
     private final CommentService commentService;
     private final UserService userService;
+    private final BlogService blogService;
 
     @PostMapping("/comment/add")
     public String addComment(@ModelAttribute("comment") CommentRequestDto commentRequestDto,
-                             @RequestParam("userId") long userId) {
+                             @RequestParam("userId") long userId,
+                             @RequestParam("blogId") long blogId) {
         Optional<User> userById = userService.findUserById(userId);
         if (userById.isPresent()){
             User user = userById.get();
@@ -33,7 +37,12 @@ public class CommentController {
                     .user(user)
                     .build();
             commentService.save(comment);
-            return "redirect:/";
+            Optional<Blog> blogById = blogService.findById(blogId);
+            if (blogById.isPresent()){
+                Blog blog = blogById.get();
+                blog.setComment(comment);
+            }
+            return "redirect:/singleBlog?id="+blogId;
         }
         return "redirect:/loginPage";
     }
